c74f532cf0d200c36874237b163704eb99ed421c
Ali Husain <alihusain.or@gmail.com>
2026-03-02 01:14:59 +0530

    changes

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as Hero } from './Hero';
-export { default as FeatureCards } from './FeatureCards';
-export { default as Platforms } from './Platforms';
-export { default as Features } from './Features';
-export { default as ComponentShowcase } from './ComponentShowcase';
-export { default as Animations } from './Animations';
-export { default as CTA } from './CTA';
-export { default as Footer } from './Footer';
